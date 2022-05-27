a386c37fd4bb6735808978fc875c9870b854c543
Chris Szczechowicz <info@cjszczechowicz.com>
2022-05-26 23:59:58 -0400

    Add request outline

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -18,13 +18,15 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
-
+// your first API endpoint...
+app.get("/api/:date", function (req, res) {
+  req.json({ unix: date });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
