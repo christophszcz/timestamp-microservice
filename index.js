f98e9245892a02ef52f8f82e8f4f8d907dab6f1f
Chris Szczechowicz <info@cjszczechowicz.com>
2022-05-28 23:56:13 -0400

    Add missing ? in endpoint path

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ app.get("/", function (req, res) {
 });
 
 // your first API endpoint...
-app.get("/api/hello", function (req, res) {
+app.get("/api/hello", function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.get("/api/:date", function(req, res, next) {
+app.get("/api/:date?", function(req, res, next) {
   next();
 }, function(req, res) {
   const inputDate = new Date(req.params.date);
